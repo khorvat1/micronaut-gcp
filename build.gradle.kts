8690453a17c0f423aa1a712d70c28bed6c0020ff
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-03 11:28:58 +0200

    kotest 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ tasks {
 graalvmNative.toolchainDetection.set(false)
 micronaut {
     runtime("netty")
-    testRuntime("kotest")
+    testRuntime("kotest5")
     processing {
         incremental(true)
         annotations("hr.khorvat.*")
